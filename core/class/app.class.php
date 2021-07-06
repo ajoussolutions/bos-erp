ae8c2dbe25ce5f4d148508081d62a73c97e66002
AJOUS <andy.jousten@live.de>
2021-07-06 23:51:51 +0200

    Change Description

--- core/class/app.class.php
+++ core/class/app.class.php
@@ -3,15 +3,14 @@ final class App{
 
     public static $appname = "BOS ERP";
     private static $applog=array();
-    /*
-    Initialize the Application
-    */
+
+    /**
+     *Initialize Application
+     */
     public static function Init(){
 
     }
-    /*
-    Add Messages to the Applog
-    */
+
     public static function Log($msg){
         self::$applog[]=$msg;
     }
